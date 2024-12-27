173cd443db9b5b082f6a52d6459e9ca279a2a24b
Taichi Ichijo <s23C1015UT@s.chibakoudai.jp>
2024-12-27 15:03:59 +0900

    kadai2

--- launch/talk_listen.launch.py
+++ launch/talk_listen.launch.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightTest: 2024 Taichi Ichijo
+# SPDX-License-Identifier: BSD-3-Clause
+
 import launch
 import launch.actions
 import launch.substitutions
--- test/test.bash
+++ test/test.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
+# SPDX-FileCopyrightTest: 2024 Taichi Ichijo
+# SPDX-License-Identifier: BSD-3-Clause
 
 dir=~
 [ "$1" != "" ] && dir="$1"
